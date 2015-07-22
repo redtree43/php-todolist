f49f91e120f3592ad21318b53dd00b2953d59bc0
Roger <gleesonrj@hotmail.com>
2015-07-21 19:28:55 -0500

    Added ability to add items

---
+++ todolist.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['list'])) {
+	$_SESSION['list'] = array();
+	}
+	
+if (isset($_POST['submit'])) {
+	array_push($_SESSION['list'],
+				$_POST['todo']);
+	
+}
+
+?>
+
+<form 	action="index.php"
+		method="POST">
+		
+<input type="text" name="todo">
+<input type="submit" name="submit">
+
+</form>
+
+<?php
+
+foreach ($_SESSION['list'] as $value) {
+    echo "<li>$value</li>";
+}
+?>
